406e705e9eafcbd5032933b7529aa634d70c459f
sveblu <svbluem@edu.aau.at>
2025-11-19 12:38:14 +0100

    Update train_tiny_imagenet_mobilenet_full_augs.py

--- ml-training/scripts/train_tiny_imagenet_mobilenet_full_augs.py
+++ ml-training/scripts/train_tiny_imagenet_mobilenet_full_augs.py
@@ -225,7 +225,7 @@ def make_datasets(
     train_ds = (
         train_files
         .flat_map(_augment_from_path)
-        .shuffle(20_000)
+        .shuffle(4_000)
         .batch(batch_size, drop_remainder=True)
         .prefetch(AUTOTUNE)
     )
